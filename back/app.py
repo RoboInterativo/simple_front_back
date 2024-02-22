7a02f4f6ac9355fe4e540bdb88e060aed76ee714
chiefexb <chief_exb@mail.ru>
2024-02-22 20:30:42 +0300

    123

--- back/app.py
+++ back/app.py
@@ -1,21 +1,22 @@
 from flask import Flask
 import json
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<p>Hello, World!</p>"
+def index(name=None):
+    return render_template('hello.html', name=name)
 
 @app.route("/api/")
 def api():
-    a=[{ "id",1,
+    a=[{ "id":1,
         "name":"Alex",
         "last_name":"Shilo",
         "salary": 50000},
-        {  "id",2,
+        {  "id":2,
             "name":"Serg",
             "last_name":"Shilo",
-            "salary": 10000},
+            "salary": 10000}
         ]
     return a
