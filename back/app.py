79ed8ea3552b1253f4f12dd4a97cbb1a493f854e
chiefexb <chief_exb@mail.ru>
2024-02-22 20:41:01 +0300

    123

--- back/app.py
+++ back/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('index.html', name=name)
 
 @app.route("/api/")
 def api():
